39caa2e9089700b20a4528920bd5a81290168f34
riccardotorre <riccardo.torre@gmail.com>
2023-10-13 10:00:00 +0200

    Modified gpu test script

---
+++ gpu_test/gpu_test_train_test.py
@@ -0,0 +1,30 @@
+import numpy as np
+import tensorflow as tf
+
+# Load the data from the local npz file
+with np.load('mnist.npz') as f:
+    x_train, y_train = f['x_train'], f['y_train']
+    x_test, y_test = f['x_test'], f['y_test']
+
+# Normalize the data
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Build a simple neural network model
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.2),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+# Train the model
+model.fit(x_train, y_train, epochs=5)
+
+# Evaluate the model
+model.evaluate(x_test, y_test)
+
