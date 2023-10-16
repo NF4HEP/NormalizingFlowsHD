b47034521297788a89ec65332e0b93048d93dd58
Riccardo Torre <rtorre@teogpu02.ge.infn.it>
2023-10-16 09:18:05 +0200

    Sync from teogpu

--- CMoG/Mains/MsplineN/c_Main_MsplineN_teogpu01.py
+++ CMoG/Mains/MsplineN/c_Main_MsplineN_teogpu01.py
@@ -2,7 +2,7 @@
 ######################################### Initialize #########################################
 ##############################################################################################
 
-visible_devices = [0,1,2,3]
+visible_devices = [0,1]
 import datetime
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+":", "Importing os...")
 import os
@@ -12,9 +12,18 @@ print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+":", "Importing a
 if not any("ipykernel" in arg for arg in sys.argv):
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("-v", "--visible_devices", help="Set visible devices", nargs='*', type=int, default=visible_devices)
+    parser.add_argument("-v", "--visible_devices", help="Set visible devices", nargs='*', type=list, default=visible_devices)
     args = parser.parse_args()
     visible_devices = args.visible_devices if args.visible_devices else visible_devices
+    if len(visible_devices) == 0:
+        visible_devices = int(visible_devices)
+    elif len(visible_devices) == 1:
+        if len(visible_devices[0]) == 0:
+            visible_devices = int(visible_devices[0])
+        else:
+            visible_devices = [int(i) for i in visible_devices[0]]
+    else:
+        visible_devices = [int(i) for i in visible_devices]
 print("Visible devices:", visible_devices)
 print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")+":", "Importing timer from timeit...")
 from timeit import default_timer as timer
--- CMoG/Mains/MsplineN/launch_runs.sh
+++ CMoG/Mains/MsplineN/launch_runs.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Declare machine name
+declare -a machine_name=("teogpu02")
+
 # Declare an array of visible devices you'd like to use
-declare -a visible_devices_list=("0" "1" "2")
+declare -a visible_devices_list=("03" "13" "23")
 
 # Declare an array of run numbers
-declare -a run_numbers=("9")
+declare -a run_numbers=("1")
 
 # Loop through arrays and launch jobs
 for run_num in "${run_numbers[@]}"
@@ -12,14 +15,14 @@ do
     for vis_dev in "${visible_devices_list[@]}"
     do
         echo "Launching run RUN_NUMBER=$run_num on device VISIBLE_DEVICES=$vis_dev"
-        echo "Script string: nohup python c_Main_MsplineN.py --visible_devices $vis_dev >> ../../Outputs/MsplineN/run_gpu_${vis_dev}_replica_${run_num}.out 2>&1 &"
+        echo "Script string: nohup python c_Main_MsplineN_${machine_name}.py --visible_devices $vis_dev >> ../../Outputs/MsplineN/run_${machine_name}_${vis_dev}_replica_${run_num}.out 2>&1 &"
 
-        nohup python c_Main_MsplineN.py --visible_devices $vis_dev >> ../../Outputs/MsplineN/run_gpu_${vis_dev}_replica_${run_num}.out 2>&1 &
+        nohup python c_Main_MsplineN_${machine_name}.py --visible_devices $vis_dev >> ../../Outputs/MsplineN/run_${machine_name}_${vis_dev}_replica_${run_num}.out 2>&1 &
 
         # Capture the PID of the last background job
         pid=$!
         echo "Launched with PID $pid"
 
-        sleep 10  # Adding sleep to prevent possible race conditions
+        sleep 60  # Adding sleep to prevent possible race conditions
     done
 done
